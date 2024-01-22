7d1b20878d510a445892d2d1189e06ec820dc2bb
David Neal <davidjn2817@gmail.com>
2024-01-22 06:25:55 -0600

    working lap counter, and timed power

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
@@ -32,33 +36,58 @@ var (
 		"black":  21,
 	}
 
-	lap_count_unix map[string][]int64
-	lap_count_time map[string][]float64
+	lap_count_unix = make(map[string][]int64)
+	lap_count_time = make(map[string][]float64)
+	lane_power_mu  sync.Mutex
+	mu             sync.Mutex
 
 	min_lap_time float64 = 2.1
+
+	view *tview.Box
+	app  *tview.Application
 )
 
-func lap(lane string, unix_now int64) {
+const refreshInterval = 500 * time.Millisecond
+
+func power_all(power bool, minutes int) {
+	for lane, gpio_pin := range lane_power_layout {
+		if power {
+			go power_lane(lane, gpio_pin, power, minutes)
+		} else {
+			power_lane(lane, gpio_pin, power, minutes)
+		}
+
+	}
+}
+
+func lap(lane string, unix_now int64) bool {
 	//s := a[len(a)-1]
-	lane_laps_unix := lap_count_unix[lane]
-	if len(lane_laps_unix) > 0 {
-		last_lap_unix := lane_laps_unix[:len(lane_laps_unix)-1]
-		fmt.Println("\n", lane, "last lap:", last_lap_unix)
-	} else {
-		fmt.Println("\n", lane, "First lap:", unix_now)
+	var last_lap_unix int64
+	var lap_time_s float64
+	if len(lap_count_unix[lane]) == 0 {
+		//log.Println("First lap on", lane)
+		lap_count_unix[lane] = append(lap_count_unix[lane], unix_now)
+		return true
 	}
 
-	lane_laps_unix = append(lane_laps_unix, unix_now)
-	fmt.Println(lane, "Laps:", lane_laps_unix)
-	fmt.Println(lap_count_unix)
+	last_lap_unix = lap_count_unix[lane][len(lap_count_unix[lane])-1]
+	mu.Lock()
+	defer mu.Unlock()
 
-	//if lap_time > min_lap_time {
-	//	lap_count[lane] = append(lap_count[lane], unix_now)
-	//	fmt.Println(lane, "lap:", unix_now)
-	//	fmt.Println(lap_count)
-	//} else {
-	//	fmt.Println(lane, "lap (not counted):", unix_now)
-	//}
+	lap_time_ms := unix_now - last_lap_unix
+	lap_time_s = float64(lap_time_ms) / 1000.00
+	if lap_time_s > min_lap_time {
+		//log.Println("Counted lap on", lane, lap_time_s)
+		// TODO: prevent fatal error: concurrent map read and map write
+		lap_count_unix[lane] = append(lap_count_unix[lane], unix_now)
+		lap_count_time[lane] = append(lap_count_time[lane], lap_time_s)
+
+	} else {
+		//log.Println("Fast lap NOT counted on", lane, lap_time_s)
+		return false
+	}
+	fmt.Println(lap_count_time)
+	return true
 }
 
 func lapcounter(lane string, gpio_pin int) {
@@ -71,41 +100,76 @@ func lapcounter(lane string, gpio_pin int) {
 	pin.Input()
 	pin.PullUp()
 	pin.Detect(rpio.FallEdge)
-	fmt.Println("Starting Lap Counter on", lane, "Pin:", gpio_pin)
+	//log.Println("Starting Lap Counter on", lane, "Pin:", gpio_pin)
 	for {
 		if pin.EdgeDetected() {
 			lap(lane, time.Now().UnixMilli())
-
 		}
-		time.Sleep(2 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}
 }
 
-func power_lane(lane string, gpio_pin int, power bool) {
+func power_lane(lane string, gpio_pin int, power bool, minutes int) {
 	pin := rpio.Pin(gpio_pin)
 	if err := rpio.Open(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	defer rpio.Close()
 	pin.Output()
 
 	if power {
 		pin.High()
-		fmt.Println(lane, "Powered on, pin:", gpio_pin)
+		log.Println("Powered ON", lane, "lane, pin", gpio_pin)
+		if minutes > 0 {
+			time.Sleep(time.Duration(minutes) * time.Minute)
+			log.Println("Power OFF, timeout", lane, "lane, pin", gpio_pin)
+			pin.Low()
+
+		}
 	} else {
 		pin.Low()
-		fmt.Println(lane, "Powered off, pin:", gpio_pin)
+		//log.Println("Powered OFF", lane, "lane, pin", gpio_pin)
+	}
+}
+
+func lapcounts(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
+	var lane_stats string
+	for lane := range lane_lap_layout {
+		lap_count := len(lap_count_time[lane])
+		lane_stat := lane_stats + lane + strconv.Itoa(lap_count) + "\n"
+		tview.Print(screen, lane_stat, x, height/2, width, tview.AlignCenter, tcell.ColorLime)
+	}
+
+	return 0, 0, 0, 0
+}
+
+func refresh() {
+	tick := time.NewTicker(refreshInterval)
+	for {
+		select {
+		case <-tick.C:
+			app.Draw()
+		}
 	}
 }
 
 func main() {
-	for lane, gpio_pin := range lane_power_layout {
-		power_lane(lane, gpio_pin, true)
-		time.Sleep(200 * time.Millisecond)
-		power_lane(lane, gpio_pin, false)
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	//power_all(false, 0)
+	power_all(true, 1)
 
+	for lane, gpio_pin := range lane_lap_layout {
+		go lapcounter(lane, gpio_pin)
 	}
+
+	//app = tview.NewApplication()
+	//view = tview.NewBox().SetBorder(true).SetTitle("Hello, world!").SetDrawFunc(lapcounts)
+
+	//go refresh()
+	//if err := app.SetRoot(view, true).Run(); err != nil {
+	//	panic(err)
+	//}
+
 	var wg = &sync.WaitGroup{}
 	for lane, gpio_pin := range lane_lap_layout {
 		go lapcounter(lane, gpio_pin)
